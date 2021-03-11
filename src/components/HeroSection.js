3f5def053f383b232cb9d6482691e05df5f91d83
chaithanya <chaithanyakumar.karuna@charter.com>
2021-03-10 19:42:29 -0700

    one more last time

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,6 @@ import "./HeroSection.css";
 const HeroSection = ({ title, content = null }) => {
   return (
     <div className="hero-container">
-      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>{title}</h1>
       <p>{content}</p>
     </div>
