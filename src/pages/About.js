c30810e17f7ce88f6c2785c62dea19c4068e4c32
amruta <amruta@192.168.0.7>
2021-03-18 22:51:03 -0600

    Modified Content

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,84 +7,43 @@ const About = () => {
   const profiles = [
     {
       name: "Amruta Varshini",
-      post: "Founder (CEO)",
+      post: "Founder, CEO",
       avatar: "/images/Amruta.jpg",
       detail: `
-        Our Chief Executive Officer, Amruta, is enthusiastic in her work for Is-Kool because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients.
+       Amruta Varshini founded IsKool in 2019 with a strong desire to launch the platform, 
+       uniting creative and technical minds. She applies strategic thinking to align the product, 
+       business to meet the user goals perfectly. Her experience in a wide variety of business sectors, 
+       geographic locations bring innovative ideas to the table. </br> </br>
+	   Amruta Varshini is an Alumni of PSG College of Technology, India.
       `,
     },
     {
       name: "Chaithanya Karuna",
-      post: "Director Of Development",
+      post: "Director of Development",
       avatar: "/images/chaithanya.jpeg",
       detail: `
-        Our Director Of Developent, Chaithanya Karuna, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients.
+        Chaithanya Kumar is an experienced developer with work experience in different 
+        fortune 500 companies as a technical fellow. 
+        His deliverables are efficiently servicing millions of customers across the globe. 
+        He is a true leader, visionary, passionate coder, teacher, and a great asset to IsKool,
+        who has been with us right from the launch. </br> </br>
+		Chaithanya Kumar has a Master’s degree in Computer Technology from EIU, Illinois,
+		and he earned his Master’s in Business Administration from CU Boulder.
+
       `,
     },
     {
-      name: "Pavan Teja",
+      name: "Pavan Kumar Teja",
       post: "Director of Operations",
       avatar: "/images/teja.jpg",
       detail: `
-        Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients.
-        Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients.
-        Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients.
-        Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients.
-      `,
+        Pavan Kumar Theja is the Head of Operations and Control, and runs our operations from India.
+        Animator at heart, Pavan is a creator, designer, and the most enthusiastic
+         member of IsKool. 
+		From a technical, design, user, or client perspective — he's there for 
+		you, fully aware of the context of a project. </br> </br>
+		Pavan Kumar Theja is an Alumni of Amrita Vishwa Vidyapeetham University, India.
+          `,
     },
   ];
   return (
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -6,71 +6,64 @@ import ProductCards from "../components/ProductCards";
 const Products = () => {
   const productArr = [
     {
-      title: "is-kool",
+      title: "IsKool",
       avatar: "/images/is-kool.jpeg",
       description: `
-        Is-kool designed to make student, teachers and parents interactions lively and easy.
-        </br></br>
-        Simple and easy to access and use.
-        Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients. Our Director of Operations, Paven Teja, is enthusiastic in his work for Qualfon because
-        of the person-focused motivation of the company. He values that the company's overall
-        success ripples into the success of our emloyees, their families, and our clients.`,
+        Our product IsKool is a simple, cool way for Parents, 
+        Teachers, and Students to use one web application 
+        for all their school activities. </br> </br>
+		Students </br> </br>
+		•	Manage Assignments and Submissions </br>
+		•	Class Timetables, Due Dates </br>
+		•	Quizzes, Tests </br>
+		•	Evaluations, Grades </br> </br>
+		Teachers </br> </br>
+		•	Mange Curriculum, Modules </br>
+		•	Manage Schedules, Custom Dashboards </br>
+ 		•	School Feeds </br>
+		•	Student Activities and Monitoring </br> </br>
+		Parents </br> </br>
+		•	Registration </br>
+		•	Payments </br>
+		•	Kid’s activities and Monitoring </br> </br>
+		Additional Features </br> </br>
+		•		Customizable Admin options
+        `,
     },
     {
-      title: "live-kool",
+      title: "LiveKool",
       avatar: "/images/live-kool.jpeg",
       description: `
-      Live-kool designed to make partment communities life lively and easy.
-      </br></br>
-      Simple and easy to access and use. `,
+      Our product Live Kool is a simple, cool Apartment Renters application
+      where owners and renters can manage their rental activities with ease.</br> </br>
+		Owners</br> </br>
+		•	Manage and Screen Applications </br>
+		•	Resident Management </br>
+		•	Customize Lease	</br>
+		•	Apartment management and Operations</br> </br>
+		Lessees</br> </br>
+		•	Apartment Listing </br>
+		•	Tour Apartments </br>
+		•	Renters Payments </br>
+		•	Apartment Maintenance </br> </br>
+		Additional Features</br> </br>
+		•	Credit reports </br>
+		•	Rent Price Analysis </br>
+       `,
     },
     {
-      title: "docit",
+      title: "DocKool",
       avatar: "/images/Docit.jpeg",
       description: `
-      Store all your important family files in one place. One access for entire family, easy to access and use.
+      Our product Doc – Kool is a simple, cool Document collaboration application
+       where your teams can manage notes, documents, projects, deliverables, 
+       tech specs, guides all in one place and fast. </br> </br>
+		Features </br> </br>
+		•	Multiple Workspaces </br>
+		•	Auto Formatting </br>
+		•	Automated Tables </br>
+		•	Document Themes and Branding.</br>
+      
       `,
     },
   ];
